b35db55967eeeb3d47b481156398f47576ee389d
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2020-11-26 22:17:19 +0200

    Refactor Preprocessor

--- postprocessor/postprocessor.py
+++ postprocessor/postprocessor.py
@@ -12,7 +12,7 @@ class Postprocessor:
     def postprocess(self):
         self.__remove_intersected()
         img_text = cv.drawContours(
-            self.__img.copy(), self.__ccs_non_text, -1, 0, -1)
+            self.__img.copy(), [cc.get_contour() for cc in self.__ccs_non_text], -1, 0, -1)
         return self.__ccs_text, self.__ccs_non_text, img_text
 
     def __remove_intersected(self):
@@ -25,7 +25,7 @@ class Postprocessor:
     def __get_non_text_image(self):
         non_text_image = self.__img.copy()
         for cc in self.__ccs_text:
-            x, y, w, h = cv.boundingRect(cc)
+            x, y, w, h = cc.get_rect()
             cv.rectangle(non_text_image, (x, y), (x + w, y + h), (0, 0, 0), -1)
 
         return non_text_image
@@ -39,7 +39,7 @@ class Postprocessor:
     def __cc_intersect_with_non_text_img(self, cc, non_text_img):
         blank = np.zeros(self.__img.shape[0:2])
 
-        x, y, w, h = cv.boundingRect(cc)
+        x, y, w, h = cc.get_rect()
         img = cv.rectangle(blank.copy(), (x, y), (x + w, y + h), 255)
 
         intersection = np.logical_and(img, non_text_img)
